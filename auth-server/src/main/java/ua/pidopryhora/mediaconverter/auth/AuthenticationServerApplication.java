56a315048bc84d119cf297d27764b7f4042dc94d
yevhenpi <yevhen.pidopryhora@gmail.com>
2025-01-31 18:10:57 +0200

    AuthServer module created

---
+++ auth-server/src/main/java/ua/pidopryhora/mediaconverter/auth/AuthenticationServerApplication.java
@@ -0,0 +1,12 @@
+package ua.pidopryhora.mediaconverter.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServerApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(AuthenticationServerApplication.class, args);
+    }
+}
